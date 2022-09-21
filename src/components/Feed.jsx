9ec06bd03983f12a5afcef02bca3034c22114048
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-21 06:28:39 +0300

    Feed and its contents

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffct } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import Sidebar from './Sidebar'
+import Videos from './Videos'
 
 const Feed = () =>
 (
@@ -13,8 +14,15 @@ const Feed = () =>
       }}>
       <Sidebar />
       <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
-        Copyright 2022
+        Copyright &copy; 2022.
       </Typography>
+    </Box>
+
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        <span style={{ color: '#FC1503' }}>videos</span>
+      </Typography>
+      <Videos />
     </Box>
   </Stack>
 )
