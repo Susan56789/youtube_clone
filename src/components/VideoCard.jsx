2821fae29a5eaff612ceb2ece7c7a1425aee6d03
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-24 08:40:15 +0300

    modified videocard and its contents

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
-import { CheckCirlce } from '@mui/icons-material'
+import { CheckCircle } from '@mui/icons-material'
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 
-const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
-    console.log("videoId:", videoId)
+const VideoCard = ({ video }) => {
+
     return (
-        <Card>
-            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+            <Link to={video.videoId ? `/video/${video.videoId}` : demoVideoUrl}>
                 <CardMedia
-                    image={snippet?.thumbnails?.high?.url}
-                    alt={snippet?.title}
+                    image={video?.thumbnails[0]?.url || demoThumbnailUrl}
+                    alt={video?.title || demoVideoTitle}
                     sx={{ height: 180, width: 358 }}
                 />
             </Link>
             <CardContent
-                sx={{ backgroundColor: '#1e1e1e', height: '100px' }}
-            />
+                sx={{ backgroundColor: '#1e1e1e', height: '100px' }}>
+                <Link to={video.videoId ? `/video/${video.videoId}` : demoVideoUrl}>
+                    <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
+                        {video?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                    </Typography>
+                </Link>
+                <Link to={video.author.channelId ? `/channel/${video.author.channelId}` : demoChannelUrl}>
+                    <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+                        {video?.author.title.slice(0, 60) || demoChannelTitle.slice(0, 60)}
+                        <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+                    </Typography>
+                </Link>
+            </CardContent>
+
         </Card>
     )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,18 +5,19 @@ import VideoCard from './VideoCard'
 import ChannelCard from './ChannelCard'
 
 const Videos = ({ videos }) => {
-    console.log(videos)
+    //console.log(videos)
     return (
         <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-            {videos.map((item, idx) => (
-                <Box key={idx}>
-                    {item.videoId && <VideoCard video={item} />}
-                    {item.channelId && <ChannelCard channelDetail={item} />}
+
+            {videos.map((vid, index) => (
+                < Box key={index} >
+
+                    {vid.video?.videoId && <VideoCard video={vid.video} />}
+                    {/** {vid.channel.channelId && <ChannelCard channelDetail={vid.channel} />} */}
                 </Box>
             ))
             }
-
-        </Stack>
+        </Stack >
     )
 }
 
